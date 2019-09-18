cf18d1610de9ba95925f68b029481cf6a9cfeb08
Sebastián Arezlon Spartano <arezlonsp@gmail.com>
2019-09-18 15:32:16 -0300

    update4
    footerfinal

--- application/views/footer2.php
+++ application/views/footer2.php
@@ -8,22 +8,22 @@
 
 		<ul class="ul-usuario navbar-nav mx-auto"style="margin:0px;padding: 0px">
 			<li class="nav-item active mx-auto">
-				<a class="nav-link blanco-trans evexnod-navbar-font" href="#">PLACEHOLDER1</a>
+				<a class="nav-link blanco-trans evexnod-navbar-font" style="font-size: 16px;padding: 6px"href="#"><i class="fas fa-question-circle"></i>NOSOTROS</a>
 			</li>
 			<li class="nav-item active mx-auto">
-				<a class="nav-link blanco-trans evexnod-navbar-font" href="#">PLACEHOLDER2</a>
+				<a class="nav-link blanco-trans evexnod-navbar-font" style="font-size: 16px;padding: 6px"href="#"><i class="fas fa-cubes"></i>PROYECTOS</a>
 			</li>
 			<li class="nav-item active mx-auto">
-				<a class="nav-link blanco-trans evexnod-navbar-font" href="#">PLACEHOLDER3</a>
+				<a class="nav-link blanco-trans evexnod-navbar-font" style="font-size: 16px;padding: 6px"href="#"><i class="fas fa-cog"></i>SOPORTE</a>
 			</li>
 			<li class="nav-item active mx-auto">
-				<a class="nav-link blanco-trans evexnod-navbar-font" href="#">RS1</a>
+				<a class="nav-link blanco-trans evexnod-navbar-font" style="font-size: 20px;padding: 3px"href="#"><i class="fab fa-instagram"></i></a>
 			</li>
 			<li class="nav-item active mx-auto">
-				<a class="nav-link blanco-trans evexnod-navbar-font" href="#">RS2</a>
+				<a class="nav-link blanco-trans evexnod-navbar-font" style="font-size: 20px;padding: 3px"href="#"><i class="fab fa-facebook-square"></i></a>
 			</li>
 			<li class="nav-item active mx-auto">
-				<a class="nav-link blanco-trans evexnod-navbar-font" href="#">RS3</a>
+				<a class="nav-link blanco-trans evexnod-navbar-font" style="font-size: 20px;padding: 3px"href="#"><i class="fab fa-twitter-square"></i></a>
 			</li>
 		</ul>
 	</nav>
@@ -33,10 +33,12 @@
 <div class="row evexnod-navbar-font" style="padding: 0px;margin:0px;margin-top: -30px">
 	<div class="col-sm-1"></div>
 	<div class="col-sm-10 text-center">
-		<p class="font-arez"style="font-size: 14px">NEHUEN VILA - EVEXNOD STORE 2019</p>
+		<p class="font-arez"style="font-size: 14px">
+		©2019 EVEXNOD STORE - TODOS LOS DERECHOS RESERVADOS</p>
 	</div>
 	<div class="col-sm-1"></div>
 </div>
+
 
 
 
