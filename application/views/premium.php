88a0aaa74e4eeb6217b0820fc0ee9c761803a6a7
Arezlon <50326537+Arezlon@users.noreply.github.com>
2019-11-02 15:58:00 -0300

    update15(
    correcciones

--- application/views/premium.php
+++ application/views/premium.php
@@ -1,3 +1,32 @@
+<style>
+	@media (min-width: 1199px){
+		.responsiveTitle{
+			font-size:22px;
+		}
+	}
+	@media (min-width: 992px) and (max-width: 1199px){
+		.responsiveTitle{
+			font-size:22px;
+		}
+	}
+	@media (min-width: 768px) and (max-width: 991px){
+		.responsiveTitle{
+			font-size:16px;
+		}
+	}
+	@media (min-width: 576px) and (max-width: 767px){
+		.responsiveTitle{
+			font-size:12px;
+		}
+	}
+	@media (max-width: 575px){
+		.responsiveTitle{
+			display: none;
+		}
+	}
+</style>
+
+
 <br>
 <div class="row contenedor d-none d-sm-block" style="background-color: transparent;height: 100%">
 	<div class="col-sm-12">
@@ -7,33 +36,33 @@
 		<br>
 		<div class="row" style="height: 100%">
 			<div style="height: 220px;padding: 0px 10px 10px 10px;background-color: white"class="col-sm-3 text-center">
-				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);padding: 25px">
-					
-					<h5 class="evexnod-expositor-font" style="color:white">Acceso unico</h5>
+				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);display:flex;justify-content:center;align-items:center;">
+
+					<h5 class="evexnod-expositor-font responsiveTitle" style="color:white">Acceso unico</h5>
 				</div>
 				<br>
 				<p style="margin: 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit!</p>
 			</div>
 			<div style="height: 220px;padding: 0px 10px 10px 10px;background-color: white"class="col-sm-3 text-center">
-				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);padding: 25px">
-					
-					<h5 class="evexnod-expositor-font" style="color:white"> Recompensas Exclusivas</h5>
+				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);display:flex;justify-content:center;align-items:center;">
+
+					<h5 class="evexnod-expositor-font responsiveTitle" style="color:white"> Recompensas Exclusivas</h5>
 				</div>
 				<br>
 				<p style="margin: 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 			</div>
 			<div style="height: 220px;padding: 0px 10px 10px 10px;background-color: white"class="col-sm-3 text-center">
-				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);padding: 25px">
-					
-					<h5 class="evexnod-expositor-font" style="color:white">Perfil exclusivo</h5>
+				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);display:flex;justify-content:center;align-items:center;">
+
+					<h5 class="evexnod-expositor-font responsiveTitle" style="color:white">Perfil exclusivo</h5>
 				</div>
 				<br>
 				<p style="margin: 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 			</div>
 			<div style="height: 220px;padding: 0px 10px 10px 10px;background-color: white"class="col-sm-3 text-center">
-				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);padding: 25px">
-					
-					<h5 class="evexnod-expositor-font" style="color:white">Sin limites</h5>
+				<div style="height: 40%; width: 100%;background-color: rgba(59, 134, 209, 0.5);display:flex;justify-content:center;align-items:center;">
+
+					<h5 class="evexnod-expositor-font responsiveTitle" style="color:white">Sin limites</h5>
 				</div>
 				<br>
 				<p style="margin: 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
